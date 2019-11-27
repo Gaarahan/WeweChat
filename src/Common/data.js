9ffa9014d0a7797a94a52f382ab366046237c2e8
gaarahan <815199630@qq.com>
2019-11-27 22:15:10 +0800

    init project

---
+++ src/Common/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    title: 'App归档小组',
+    message: 'Gaara: I want my tears back...'
+  },
+  {
+    id: 2,
+    title: 'App归档小组',
+    message: 'Gaara: I want my tears back...'
+  },
+  {
+    id: 3,
+    title: 'App归档小组',
+    message: 'Gaara: I want my tears back...'
+  },
+  {
+    id: 4,
+    title: 'App归档小组',
+    message: 'Gaara: I want my tears back...'
+  },
+  {
+    id: 5,
+    title: 'App归档小组',
+    message: 'Gaara: I want my tears back...'
+  },
+  {
+    id: 6,
+    title: 'App归档小组',
+    message: 'Gaara: I want my tears back...'
+  },
+]
